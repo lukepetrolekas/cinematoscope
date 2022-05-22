cc1235c169720f0935ae68e464c413b2fccf3627
Luke Petrolekas <reclarion@gmail.com>
2022-05-21 20:18:34 -0400

    Initial commit

---
+++ tests/test_skeleton.py
@@ -0,0 +1,25 @@
+import pytest
+
+from cinematoscope.skeleton import fib, main
+
+__author__ = "Luke Petrolekas"
+__copyright__ = "Luke Petrolekas"
+__license__ = "MIT"
+
+
+def test_fib():
+    """API Tests"""
+    assert fib(1) == 1
+    assert fib(2) == 1
+    assert fib(7) == 13
+    with pytest.raises(AssertionError):
+        fib(-10)
+
+
+def test_main(capsys):
+    """CLI Tests"""
+    # capsys is a pytest fixture that allows asserts agains stdout/stderr
+    # https://docs.pytest.org/en/stable/capture.html
+    main(["7"])
+    captured = capsys.readouterr()
+    assert "The 7-th Fibonacci number is 13" in captured.out
